72b3eadf2d82f2976924e61bddd3035174d01aaa
Kwesi Cappel <Kwesic@att.net>
2018-04-14 16:36:22 -0500

    Update ord_input_design2.sql

--- ord_input_design2.sql
+++ ord_input_design2.sql
@@ -3,44 +3,14 @@ CREATE OR REPLACE PROCEDURE p_new_ord(
 	p_store_id IN ORDERS.store_id%TYPE,
 	p_book_id IN ORDERS.book_id%TYPE)
 
-IS
+AS
 BEGIN
-
-	INSERT INTO ORDERS ("Customer_ID", "Store_ID", "Book_ID")
-	VALUES (p_customer_id, p_store_id, p_book_id);
-	
-	COMMIT;
-	
-END;
-/
-
-CREATE OR REPLACE FUNCTION f_ord_total
-
-
-
-
-
-Below is syntax option
-/*CREATE OR REPLACE TRIGGER p_ord_update
-AFTER INSERT OR UPDATE ON orders
-FOR EACH ROW
-
-DECLARE*/
-CREATE OR REPLACE TRIGGER t_ord_update
-AFTER INSERT OR 
-UPDATE OF customer_id ON ORDERS
-FOR EACH ROW
-
-DECLARE
-	v_order_no ORDERS.order_no%TYPE
-	
-	
-	
-	
-	
+	INSERT INTO ORDERS (order_no, customer_id, store_id, book_id)
+	VALUES (seq_orders_id.NEXTVAL, p_customer_id, p_store_id, p_book_id);
+END p_new_ord;
 	
 	
 ---------------------------------REPORTS----------------------------------------------------------------------------------------------------------
-[April 13th, 2018]
---Documented purpose and brief descriptions of the functionality needed to properly create an ORDERS_UPDATE package that
---receives input parameters and returns the calculated values required for transaction record keeping. 
+[April 14th, 2018]
+--Concluded scripting and a little testing of the first procedure using "p_new_ord(1,1,1)". Function call accepts 3 parameters; customer id, store id, and book id. 
+--ORDER_TOTAL and WALLET_BAL columns currently remain NULL until function is more fleshed out. 
